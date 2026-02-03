c8c1d99a1b6e45d1e5121545526b64d9e7d89a95
ThiagoLopesSantos <thiago8343@gmail.com>
2026-02-03 13:10:44 -0300

    Adicionando contqagem regressiva

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ function abrirConvite() {
         });
     }, 500);
 }
+
+// CONTAGEM REGRESSIVA
+const dataCasamento = new Date("2026-11-28T14:00:00"); // ajuste o horário depois
+
+function atualizarContagem() {
+    const agora = new Date();
+    const diferenca = dataCasamento - agora;
+
+    if (diferenca <= 0) {
+        document.querySelector(".countdown").innerHTML =
+        "<p>Chegou o grande dia! 💍</p>";
+        return;
+    }
+
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferenca / (1000 * 60 * 60)) % 24);
+    const minutos = Math.floor((diferenca / (1000 * 60)) % 60);
+    const segundos = Math.floor((diferenca / 1000) % 60);
+
+    document.getElementById("days").textContent = dias;
+    document.getElementById("hours").textContent = horas;
+    document.getElementById("minutes").textContent = minutos;
+    document.getElementById("seconds").textContent = segundos;
+}
+
+setInterval(atualizarContagem, 1000);
+atualizarContagem();
